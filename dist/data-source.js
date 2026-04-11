84fa154f9b3ba55b8e03b1668d4b3a9280027f9f
hungdzvcllll <fptjun@gmail.com>
2026-04-11 22:46:39 +0700

    Update data-source.js

--- dist/data-source.js
+++ dist/data-source.js
@@ -11,13 +11,25 @@ const DishOrder_1 = require("./model/DishOrder");
 const Dish_1 = require("./model/Dish");
 const Table_1 = require("./model/Table");
 exports.AppDataSource = new typeorm_1.DataSource({
-    type: "mysql",
-    host: "metro.proxy.rlwy.net",
-    port: 30414,
-    username: "root",
-    password: "IJOvZefoEngFdqNPQRwhiSyAVzTxbupq",
-    database: "railway",
-    synchronize: true,
+     type: "mysql",
+  host: "sql100.infinityfree.com",
+  port: 3306,
+  username: "if0_41637469",
+  password: "y6zKn7DnAmPg",
+  database: "if0_41637469_railway",
+
+  synchronize: true,
+    extra: {
+    connectionLimit: 10,          // tránh mở quá nhiều connection
+    connectTimeout: 10000,        // timeout khi connect (10s)
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0,
+  },
+
+  // 👇 cực kỳ quan trọng
+  acquireTimeout: 10000,
+  retryAttempts: 5,
+  retryDelay: 3000,
     entities: [User_1.default, TableOrder_1.default, Table_1.default, DishOrderDetails_1.DishOrderDetails, DishOrder_1.default, Dish_1.default]
 });
 //# sourceMappingURL=data-source.js.map
